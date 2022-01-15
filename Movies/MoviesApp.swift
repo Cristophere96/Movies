9dc8840f72e52fd40a60ffad9bd2f2df6c515f4c
Cristopher Escorcia <cescorcia@condorlabs.io>
2022-01-15 11:45:04 -0500

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Cristopher Escorcia on 15/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
